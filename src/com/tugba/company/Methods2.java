986f72f4ad271777fefadec08f5e1fdbbea2ee44
tugbayavuzz <tugbayavuz@yandex.com>
2020-10-09 15:04:44 +0300

    void methods

--- src/com/tugba/company/Methods2.java
+++ src/com/tugba/company/Methods2.java
@@ -3,9 +3,10 @@ package com.tugba.company;
 public class Methods2 {
     public static void main(String[] args) {
         String mesaj="Bugün hava cok güzel.";
+        System.out.println(mesaj.substring(2,5));
         String yeniMesaj=sehirVer();
         System.out.println(yeniMesaj);
-        int toplam = topla();
+        int toplam = topla(5,7);
         System.out.println(toplam);
     }
 
@@ -13,13 +14,13 @@ public class Methods2 {
         System.out.println("Eklendi");
     }
     public static void sil(){
-        System.out.println("Eklendi");
+        System.out.println("Silindi");
     }
     public static void guncelle(){
-        System.out.println("Eklendi");
+        System.out.println("Guncellendi");
     }
-    public static int topla(){
-       return 5+5;
+    public static int topla(int x,int y){
+       return x+y;
     }
     public static String sehirVer(){
         return "Ankara";
