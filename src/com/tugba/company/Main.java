84d022cd24be046b58cebb0702548fb377f6d986
tugbayavuzz <tugbayavuz@yandex.com>
2020-11-12 15:28:54 +0300

    Classes and Four Functions

--- src/com/tugba/company/Main.java
+++ src/com/tugba/company/Main.java
@@ -3,7 +3,25 @@ package com.tugba.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        //classlar referans tiptir. Reference type
+        CustomerManager customerManager = new CustomerManager();//CustomerManager Çağırma işlemi
+        CustomerManager customerManager1 =new CustomerManager();
+        customerManager=customerManager1;
+        //CustomerManager customerManager stackte  tutuluyor
+        //new CustomerManager(); heapte tutuluyor
+
+        customerManager.Add();
+        customerManager.Delete();
+        customerManager.Update();
+
+        DortIslem dortIslem = new DortIslem();
+        int sonuc = dortIslem.Topla(3,4);
+        int sonuc1=dortIslem.Cikar(3,4);
+        int sonuc2=dortIslem.Carp(3,4);
+        int sonuc3=dortIslem.Bol(3,4);
+
+        System.out.println("Toplam= "+sonuc+" Cikarma= "+sonuc1+" Carpma= "+sonuc2+" Bolme= "+sonuc3);
+
 
     }
 }
